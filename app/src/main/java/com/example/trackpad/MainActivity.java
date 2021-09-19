e3beeaf9966598d6b4f8a7c95e0dfaa667a55cfc
YeonSeong-Lee <dltjddus3@gmail.com>
2021-09-19 23:50:04 +0900

    gesture test

--- app/src/main/java/com/example/trackpad/MainActivity.java
+++ app/src/main/java/com/example/trackpad/MainActivity.java
@@ -3,48 +3,67 @@ package com.example.trackpad;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
 
+import static android.view.GestureDetector.*;
+
+
 public class MainActivity extends AppCompatActivity {
-    TextView textView;
+    TextView textView = null;
+    GestureDetector gestureDetector = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         textView = (TextView) findViewById(R.id.log);
 
         View view = findViewById(R.id.view);
         view.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
-                int action = motionEvent.getAction();
+                gestureDetector.onTouchEvent(motionEvent);
+                return true;
+            }
+        });
+        gestureDetector = new GestureDetector(this, new GestureDetector.OnGestureListener() {
+            @Override
+            public boolean onDown(MotionEvent motionEvent) {
+                textView.append("onDown Call" + "\n");
+                return true;
+            }
 
-                float curX = motionEvent.getX();
-                float curY = motionEvent.getY();
+            @Override
+            public void onShowPress(MotionEvent motionEvent) {
+                textView.append("onShowPress Call" + "\n");
+            }
 
-                if (action == MotionEvent.ACTION_DOWN) {
-                    println("손가락 눌림 : " + "X: " + curX + " Y: " + curY);
-                } else if (action == MotionEvent.ACTION_MOVE) {
-                    println("손가락 움직임 : " + "X: " + curX + " Y: " + curY);
-                } else if (action == MotionEvent.ACTION_UP) {
-                    println("손가락 뗌 : " + "X: " + curX + " Y: " + curY);
-                }
+            @Override
+            public boolean onSingleTapUp(MotionEvent motionEvent) {
+                textView.append("onSingleTapUp Call" + "\n");
+                return true;
+            }
 
+            @Override
+            public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+                textView.append("onScroll Call (" + v + " * " + v1 + ")" + "\n");
+                return true;
+            }
+
+            @Override
+            public void onLongPress(MotionEvent motionEvent) {
+                textView.append("onLongPress Call" + "\n");
+            }
+
+            @Override
+            public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+                textView.append("onFling Call (" + v + " * " + v1 + ")" + "\n");
                 return true;
             }
         });
     }
-        public void println(String data) {
-            textView.append(data + "\n ");
-        }
+}
 
-
-
-
-
-
-    }
